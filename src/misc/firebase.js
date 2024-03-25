e1bef6c82ad7f0e091cf3c22ce6630dc71cce2f1
shivanshpachauri <shivanshpachauri00@gmail.com>
2024-03-25 16:25:38 +0530

    initialised firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+const config = {
+    apiKey: "AIzaSyAd3MlmqIEO2UyhgDKY_t6C1F5Bg8ff-y0",
+    authDomain: "chat-web-app-8e950.firebaseapp.com",
+    databaseURL: "https://chat-web-app-8e950-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-8e950",
+    storageBucket: "chat-web-app-8e950.appspot.com",
+    messagingSenderId: "98572430749",
+    appId: "1:98572430749:web:fa6df87b865ac3fa2ed374"
+  };
+  
+  const app=firebase.initialiseApp(config);
+  
