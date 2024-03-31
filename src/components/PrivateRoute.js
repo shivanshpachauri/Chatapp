03505853829232dcedbbbbb50a304728f9f6e754
shivanshpachauri <shivanshpachauri00@gmail.com>
2024-03-31 13:56:31 +0530

    manage user with profile context;putrealtimesubs

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Redirect,Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { Container, Loader } from 'rsuite';
 import { useProfile } from '../context/profile.context';
 
+
 const PrivateRoute = ({children,...routeProps}) => {
-    const profile=useProfile();
-    if(!profile){
+    const {profile,isLoading}=useProfile();
+    if(isLoading&&!profile){
+      return(
+        <Container>
+          <Loader center vertical size="md" content="Loading" speed="slow"/>
+        </Container>
+      )
+    }
+    if(!profile&&!isLoading){
         return <Redirect to="/signin"/>
     }
   return (
--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { Redirect,Route } from 'react-router-dom/cjs/react-router-dom.min';
 import { useProfile } from '../context/profile.context';
-
+import { Container,Loader } from 'rsuite';
 const PublicRoute = ({children,...routeProps}) => {
-    const profile=useProfile();
+    const {profile,isLoading}=useProfile();
+    if(isLoading&&!profile){
+      return(
+        <Container>
+          <Loader center vertical size="md" content="Loading" speed="slow"/>
+        </Container>
+      )
+    }
+    if(profile&&!isLoading){
+        return <Redirect to="/signin"/>
+    }
     if(profile){
         return <Redirect to="/"/>
     }
