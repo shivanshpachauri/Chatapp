c44a4b1eff67b3aafdbef5c5f42e924b5b14e6ca
shivanshpachauri <shivanshpachauri00@gmail.com>
2024-04-23 18:15:47 +0530

    update all user references if data is updated

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -1,5 +1,3 @@
-// import React from 'react'
-
 import { Modal,Button, } from 'rsuite';
 import AvatarEditor from 'react-avatar-editor'
 import { useModalState } from "../../misc/custom-hooks";
@@ -7,6 +5,7 @@ import { useState,useRef } from 'react';
 import { database, storage } from '../../misc/firebase';
 import { useProfile } from '../../context/profile.context';
 import ProfileAvatar from '../ProfileAvatar';
+import { getUserUpdates } from '../../misc/helpers';
 const fileInputTypes='.png,.jpeg,.jpg';
 const acceptedFileTypes=['image/png','image/jpeg','image/pjpeg'];
 const isValidFile=file=>acceptedFileTypes.includes(file.type);
@@ -54,10 +53,14 @@ const AvatarUploadBtn = () => {
             cacheControl:`public,max-age=${3600*24*3}`,
            });
            const downloadUrl=await uploadAvatarResult.ref.getDownloadURL()
-           const userAvatarRef=database
-           .ref(`/profiles/${profile.uid}`)
-           .child('avatar');
-           userAvatarRef.set(downloadUrl);
+        
+        const updates = await getUserUpdates(
+            profile.uid,
+            'avatar',
+            downloadUrl,
+            database
+          );
+          await database.ref().update(updates);
            setIsLoading(false);
            window.alert('Avatar has been uploaded');
         } catch (err) {
