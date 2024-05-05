d1fec30201ae61314f273aa3fd1dcd704168846d
shivanshpachauri <shivanshpachauri00@gmail.com>
2024-05-05 14:48:03 +0530

    created attachment button with uploader component

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,57 +1,55 @@
 // import React,{Alert} from 'react'
-import { Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite'
-import { auth, database } from '../misc/firebase'
+import { Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite';
+import { auth, database } from '../misc/firebase';
 import firebase from 'firebase/app';
 // import {Alert} from 'react-native';
 
 const SignIn = () => {
-  const signInWithProvider=async (provider)=>{
-    try{
-      const {additionalUserInfo,user}=await auth.signInWithPopup(provider);
-      if(additionalUserInfo.isNewUser){
-       await database.ref(`/profiles/${user.uid}`).set({
-          name:user.displayName,
-          createdAt:firebase.database.ServerValue.TIMESTAMP,
-        })
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
       }
       window.alert('Signed in successfully');
-    }catch(err){
+    } catch (err) {
       window.alert(`Error: ${err.message}`);
     }
-    
-    
-  }
-  const onFacebookSignIn=()=>{
+  };
+  const onFacebookSignIn = () => {
     signInWithProvider(new firebase.auth.FacebookAuthProvider());
-  }
-  const onGoogleSignIn=()=>{
+  };
+  const onGoogleSignIn = () => {
     signInWithProvider(new firebase.auth.GoogleAuthProvider());
-  }
+  };
   return (
-   <Container>
-    <Grid className='mt-page'>
-      <Row>
-        <Col xs={24} md={12} mdOffset={6}>
-          <Panel>
-            <div className="text-center" >
-              <h2>Welcome to chat</h2>
-              <p>Progressive chat platform for neophytes</p>
-            </div>
-            <div className="mt-3">
-            <Button block color="blue"onClick={onFacebookSignIn}>
-            <Icon icon="facebook"/> Continue with Facebook
-            </Button>
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to chat</h2>
+                <p>Progressive chat platform for neophytes</p>
+              </div>
+              <div className="mt-3">
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                  <Icon icon="facebook" /> Continue with Facebook
+                </Button>
 
-            <Button block color="green"onClick={onGoogleSignIn}>
-            <Icon icon="google"/> Continue with Google
-            </Button>
-            </div>
-          </Panel>
-        </Col>
-      </Row>
-    </Grid>
-   </Container>
-  )
-}
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
+};
 
-export default SignIn
+export default SignIn;
