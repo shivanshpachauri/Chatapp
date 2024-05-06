b7b3892194508daffa525cdf4a588cdddee0170d
shivanshpachauri <shivanshpachauri00@gmail.com>
2024-05-06 15:24:13 +0530

    store uploaded files to the database

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -4,7 +4,7 @@ import firebase from 'firebase/app';
 import { useParams } from 'react-router';
 import { useProfile } from '../../../context/profile.context';
 import { database } from '../../../misc/firebase';
-import AttachmentBtnModal from './AttachmentBtnModal';
+import AttachmentBtnModal from './AttachmenBtnModal';
 
 function assembleMessage(profile, chatId) {
   return {
@@ -66,11 +66,38 @@ const Bottom = () => {
       onSendClick();
     }
   };
+  const afterUpload = useCallback(
+    async files => {
+      setIsLoading(true);
+      const updates = {};
+      files.forEach(file => {
+        const msgData = assembleMessage(profile, chatId);
+        msgData.file = file;
+        const messageId = database.ref('messages').push().key;
+        updates[`/messages/${messageId}`] = msgData;
+      });
 
+      const lastMsgId = Object.keys(updates).pop();
+
+      updates[`/rooms/${chatId}/lastMessage`] = {
+        ...updates[lastMsgId],
+        msgId: lastMsgId,
+      };
+
+      try {
+        await database.ref().update(updates);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        Alert.error(err.message);
+      }
+    },
+    [chatId, profile]
+  );
   return (
     <div>
       <InputGroup>
-        <AttachmentBtnModal />
+        <AttachmentBtnModal afterUpload={afterUpload} />
         <Input
           placeholder="Write a new message here..."
           value={input}
