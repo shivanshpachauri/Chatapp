420a70e4dfdaa34cb18d4c5c7817d7220d9b99d5
shivanshpachauri <shivanshpachauri00@gmail.com>
2024-05-19 16:56:43 +0530

    setup cloud functions with nvm

---
+++ functions/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const { onRequest } = require('firebase-functions/v2/https');
+const logger = require('firebase-functions/logger');
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://chat-web-app-8e950-default-rtdb.firebaseio.com',
+});
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
